8dadd701023685d0a20c3dc3aae84a5cc3a45302
Tobias Dahl <tobias.dahl@consat.se>
2020-02-11 20:00:03 +0100

    Add initial manager and items

---
+++ Assets/Source/DirectoryItem.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class DirectoryItem : MonoBehaviour
+{
+	private List<DirectoryItem> Children;
+
+	private bool isDirectory = false;
+
+	
+	private void Awake()
+	{
+		if (gameObject.name.EndsWith("/") || gameObject.name.EndsWith("\\"))
+		{
+			isDirectory = true;
+		}
+	}
+	
+	public bool IsDirectory()
+	{
+		return isDirectory;
+	}
+
+	public string GetName()
+	{
+		return gameObject.name;
+	}
+
+	public List<DirectoryItem> GetChildren()
+	{
+		return Children;
+	}
+
+	public int GetChildCount()
+	{
+		return Children.Count;
+	}
+
+	public DirectoryItem GetChild(int n)
+	{
+		return Children[n];
+	}
+
+}
---
+++ Assets/Source/TreeManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeManager : MonoBehaviour
+{
+
+	private DirectoryItem Root;
+	private static GameObject DirectoryItemPrefab;
+
+	public string InputArray;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	private bool CreateFolder(DirectoryItem parent, string name)
+	{
+		if (parent == null)
+			return false;
+		if (name == null)
+			return false;
+		return true;
+	}
+}
